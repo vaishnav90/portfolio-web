ea1e0a65e10e21a3915f42b52d495266d10d8a8d
vaishnav90 <vaishnavanand90@gmail.com>
2024-12-17 22:45:26 -0800

    finished

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 cluster=MongoClient("mongodb+srv://vaishnav:vaishnav@cluster0.tzdx0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = cluster["vaishnav"]
 collection = db["portfolio"]
+collection2 = db["ratings"]
 collection.insert_one({"name": "vaishnav", "email": "vaishnavanand90@gmail.com", "message": "hello"})
 @app.route('/')
 def home():
@@ -16,8 +17,30 @@ def contact():
     message = request.form.get('message')
     collection.insert_one({"name": name, "email": email, "message":message})
     return "Form submitted!", 200
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name', 'Anonymous') 
+    rating = request.form.get('rating', type=int)
+    opinion = request.form.get('opinion')
+    collection2.insert_one({"name": name, "rating": rating, "opinion": opinion})
+    sorted_comments = collection2.find().sort("rating", -1)
+    print(sorted_comments)
+    return render_template('form.html', sorted_comments=sorted_comments)
+    # return "Form submitted!", 200
+@app.route('/review')
+def review():
+    sorted_comments = collection2.find().sort("rating", -1)
+    return render_template('form.html', sorted_comments = sorted_comments)
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/contact2')
+def contact2():
+    return render_template('contact.html')
 if __name__ == '__main__':
     app.run(debug=True, port = 5000)
-
 
 
